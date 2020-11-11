b3abfd02e3dd263ece5b6de26cb8138dcea67a65
j23063519 <j23063519@gmail.com>
2020-11-11 10:46:37 +0800

    stucture

---
+++ signUp_signIn.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>signUp_signIn</title>
+</head>
+<body>
+  <div class="memberTitle">
+    <h1 class="memberTitle">會員中心</h1>
+  </div>
+  <div class="wrap">
+    <div class="signUpsignIn">
+      <div class="signUp">
+
+      </div>
+      <div class="signIn">
+
+      </div>
+      <div class="signUpR">
+
+      </div>
+      <div class="signInL">
+
+      </div>
+    </div>
+  </div>
+</body>
+</html>
