526741eece194dfed717bf9ec757d00fb9db2394
Jiang-Chiang <republicmarch@gmail.com>
2020-11-19 22:30:22 +0800

    20111906update

--- js/22/index.js
+++ js/22/index.js
@@ -38,23 +38,44 @@ $(function () {
                     $('#navIcons03 img').attr('src', '../img/22/index/ch02_nav_member.png');
                     break;
                 case 2:
-                    $('#logo').attr('src', '../img/22/index/ch02_header_logo.png');
+                    $('#logo').attr('src', '../img/22/index/ch03_header_logo.png');
                     $('#header').css('background-color', 'transparent');
                     $('.navGeneralAnchors').css('color', '#808F87');
-                    $('#navSepcialAnchor').css('color', '#FFFFFF');
-                    $('#navSepcialAnchor div').css('background-color', '#717E5B');
-                    $('#navIcons01 img').attr('src', '../img/22/index/ch02_nav_search.png');
-                    $('#navIcons02 img').attr('src', '../img/22/index/ch02_nav_cart.png');
-                    $('#navIcons03 img').attr('src', '../img/22/index/ch02_nav_member.png');
+                    $('#navSepcialAnchor').css('color', '#F7F7F7');
+                    $('#navSepcialAnchor div').css('background-color', '#96B3A3');
+                    $('#navIcons01 img').attr('src', '../img/22/index/ch03_nav_search.png');
+                    $('#navIcons02 img').attr('src', '../img/22/index/ch03_nav_cart.png');
+                    $('#navIcons03 img').attr('src', '../img/22/index/ch03_nav_member.png');
                     break;
                 case 3:
+                    $('#logo').attr('src', '../img/22/index/ch04_header_logo.png');
+                    $('#header').css('background-color', '#D3E4C6');
                     $('.navGeneralAnchors').css('color', '#8F8770');
+                    $('#navSepcialAnchor').css('color', '#FFFFFF');
+                    $('#navSepcialAnchor div').css('background-color', '#887664');
+                    $('#navIcons01 img').attr('src', '../img/22/index/ch04_nav_search.png');
+                    $('#navIcons02 img').attr('src', '../img/22/index/ch04_nav_cart.png');
+                    $('#navIcons03 img').attr('src', '../img/22/index/ch04_nav_member.png');
                     break;
                 case 4:
+                    $('#logo').attr('src', '../img/22/index/ch05_header_logo.png');
+                    $('#header').css('background-color', 'transparent');
                     $('.navGeneralAnchors').css('color', '#887664');
+                    $('#navSepcialAnchor').css('color', '#FFFFFF');
+                    $('#navSepcialAnchor div').css('background-color', '#717E5B');
+                    $('#navIcons01 img').attr('src', '../img/22/index/ch05_nav_search.png');
+                    $('#navIcons02 img').attr('src', '../img/22/index/ch05_nav_cart.png');
+                    $('#navIcons03 img').attr('src', '../img/22/index/ch05_nav_member.png');
                     break;
                 case 5:
+                    $('#logo').attr('src', '../img/22/index/ch06_header_logo.png');
+                    $('#header').css('background-color', '#F6EEE5');
                     $('.navGeneralAnchors').css('color', '#96908A');
+                    $('#navSepcialAnchor').css('color', '#FFFFFF');
+                    $('#navSepcialAnchor div').css('background-color', '#B6ADA5');
+                    $('#navIcons01 img').attr('src', '../img/22/index/ch06_nav_search.png');
+                    $('#navIcons02 img').attr('src', '../img/22/index/ch06_nav_cart.png');
+                    $('#navIcons03 img').attr('src', '../img/22/index/ch06_nav_member.png');
                     break;
             }
         }
