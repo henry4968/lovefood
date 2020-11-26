06582a3f98bd3f30297b3d70f85bc9a8ae0e3771
jeanshr682688 <jean.shr@gmail.com>
2020-11-26 16:15:41 +0800

    commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+
+const {
+    src , dest , watch
+} = require('gulp');
+const sass = require("gulp-sass");
+
+
+
+var paths = {
+    styles: {
+
+        src:["scss/backstage/*.scss","scss/layout/*.scss","scss/page/backstage/*.scss"] ,
+
+        dest: "css/backstage/"
+    }
+ 
+};
+
+function style() {
+    return src(paths.styles.src)
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(dest(paths.styles.dest));
+}
+exports.default = function watchs(){
+    watch(['scss/page/backstage/*.scss'], style)
+}
