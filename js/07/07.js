eec058600e8c4a65a17b89ba8f80b683cf815196
nimo <>
2020-12-01 20:03:27 +0800

    修改連結

--- js/07/07.js
+++ js/07/07.js
@@ -5,30 +5,30 @@ $(function(){
     });
 }); 
 //////////////輪播////////////
-$(document).ready(function() {
-    $('#lightSlider').lightSlider({
+$(document).ready(function () {
+    $('#carouselist').lightSlider({
+        item: 4,
+        loop: true,
         auto: true,
-        item:3,
-        loop:false,
-        slideMove:1,
+        slideMove: 1,
         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:300,
-        responsive : [
+        speed: 600,
+        responsive: [
             {
-                breakpoint:800,
+                breakpoint: 800,
                 settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
+                    item: 3,
+                    slideMove: 1,
+                    slideMargin: 6,
+                }
             },
             {
-                breakpoint:480,
+                breakpoint: 480,
                 settings: {
-                    item:2,
-                    slideMove:1
-                  }
+                    item: 2,
+                    slideMove: 1
+                }
             }
         ]
-    });  
+    });
 });
