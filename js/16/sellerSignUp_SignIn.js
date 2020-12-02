30d5db0d9335b9f15b36946c3308cd21d9424b22
jeanshr682688 <jean.shr@gmail.com>
2020-12-02 16:57:42 +0800

    1202

--- js/16/sellerSignUp_SignIn.js
+++ js/16/sellerSignUp_SignIn.js
@@ -32,15 +32,14 @@ Vue.component('seller', {
     template: `
         <div class="seller">
           <form id="seller" method="post" action="#">
-            ＊<input class="seller" type="email" placeholder="信箱" />
-            ＊<input class="seller" type="password" placeholder="密碼" />
-            ＊<input class="seller" type="password" placeholder="確認密碼" />
-            ＊<input class="seller" type="text" placeholder="公司名稱" />
+            <input class="seller" type="email" placeholder="信箱" />
+            <input class="seller" type="password" placeholder="密碼" />
+            <input class="seller" type="password" placeholder="確認密碼" />
+            <input class="seller" type="text" placeholder="公司名稱" />
             <input class="seller" type="text" placeholder="統一編號" />
-            ＊<input class="seller" type="text" placeholder="登記地址" />
-            ＊<input class="seller" type="text" placeholder="聯絡電話" />
+            <input class="seller" type="text" placeholder="登記地址" />
+            <input class="seller" type="text" placeholder="聯絡電話" />
             <button class="seller" type="submit">註冊</button>
-            <button class="changeTOmember" id="goTOmember" type="button">會員登入</button>
           </form>
         </div>
       </div>
@@ -97,12 +96,12 @@ new Vue({
 
 function doFirst() {
     // 會員登入
-    changeTOmember = document.getElementsByClassName('changeTOmember')[0];
+    // changeTOmember = document.getElementsByClassName('changeTOmember')[0];
     // 註冊
     changeTosignup = document.getElementsByClassName('changeTosignup')[0];
 
     // 會員登入(函式)
-    changeTOmember.addEventListener('click', changeTOmemberf);
+    // changeTOmember.addEventListener('click', changeTOmemberf);
     // 註冊(函式)
     changeTosignup.addEventListener('click', changeTosignupf);
 }
