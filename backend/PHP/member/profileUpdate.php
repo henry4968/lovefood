ea7dfc92f09cbdb976799c61d42418d5c348846f
Marshall Chou <joshtinhanmacbook@gmail.com>
2020-12-07 01:49:04 +0800

    Vue component props

--- backend/PHP/member/profileUpdate.php
+++ backend/PHP/member/profileUpdate.php
@@ -5,7 +5,7 @@
     $statement = $Util->getPDO()->prepare($sql);
     
     $statement->bindValue(1,$_POST['address']);
-    $statement->bindValue(2,$_POST['phone']);
+    $statement->bindValue(2,$_POST['phone1']);
     $statement->bindValue(3,$_POST['id']);
     $statement->execute();
 ?>
