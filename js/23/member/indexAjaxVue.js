73dfed083e97bb9ea83c77efc3f02c41242ecf5c
Marshall Chou <joshtinhanmacbook@gmail.com>
2020-12-07 11:55:33 +0800

    file list arrangement

---
+++ js/23/member/indexAjaxVue.js
@@ -0,0 +1,65 @@
+        const app = new Vue({
+        el: '.containerIndex',
+        data() {
+            return {
+                tableData:null,
+            }
+        },
+        
+        beforeMount() {
+            const self = this;
+            let urlParams = new URLSearchParams(window.location.search);
+            let number= urlParams.get('number');
+            let phone= urlParams.get('phone');
+            let account= urlParams.get('account');
+            let memberType = urlParams.get('memberType');
+            let pick1= urlParams.get('datePick1');
+            let pick2= urlParams.get('datePick2');
+
+                $.ajax({
+                    url:'../PHP/backStage/member/memberQuery.php',  //檔案請注意路徑,是相對於引用檔並非相對於此檔案
+                    type:'POST',
+                    data:{number,phone,account,memberType,pick1,pick2},
+                    success:function(res){
+                        console.log(res);
+                        self.tableData = res;
+                    },
+                    error:function(res){
+                        console.log(res);
+                    },
+                    dataType:"JSON",
+                    })
+                    // console.log(123);
+
+        },
+
+        methods: {
+            query(){
+                    const self = this;
+                    
+                    let number = $("input[name='number']").val();
+                    let phone = $("input[name='phone']").val();
+                    let account = $("input[name='account']").val();
+                    let memberType = $("input[name='memberType']:checked").val();
+                    let pick1 = $("input[name='datePick1']").val();
+                    let pick2 = $("input[name='datePick2']").val();
+
+                    $.ajax({
+
+                        url:'../PHP/backStage/member/memberQuery.php', //檔案請注意路徑,是相對於引用檔並非相對於此檔案
+                        data:{number,phone,account,memberType,pick1,pick2},
+                        type:'POST',
+                        dataType:'JSON',
+                        traditional: true,
+                        success: function(res){
+                            // console.log(res);
+                            self.tableData = res;
+                        },
+                        error: function(res){
+                            console.log(res);
+                        },
+                    });
+            }
+        }
+        
+    });
