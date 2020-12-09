108fb35ecc944673b7547847c21c1fca0b58f4dc
Jiang-Chiang <republicmarch@gmail.com>
2020-12-09 23:11:54 +0800

    20120902break

--- js/22/pointsDetailsAjaxVue.js
+++ js/22/pointsDetailsAjaxVue.js
@@ -2,9 +2,9 @@ const app = new Vue({
     el: '.containerPoints',
     data() {
         return {
-            tableData01: null,
-            tableData02: null,
-            tableData03: null,
+            pointsOfMember: null,
+            issanceLog: null,
+            discountLog: null,
             tableData04: null
         }
     },
@@ -24,77 +24,74 @@ const app = new Vue({
             type: 'POST',
             data: { number, account, name, phone, pick01, pick02 },
             success: function (res) {
-                self.tableData01 = res.pointsOfMember;
-                self.tableData02 = res.pointsUsing;
-                self.tableData03 = res.pointsIssance;
+                self.pointsOfMember = res.pointsOfMember;
+                self.issanceLog = res.issanceLog;
+                self.discountLog = res.discountLog;
                 console.log(res);
-                // console.log(res.pointsOfMember[0][0])
             },
             error: function (res) {
                 console.log("回傳失敗！");
-                console.log(res);
+                console.log(res.responseText);
             },
             dataType: "JSON",
         });
     },
-    updated() {
-        const self = this;
-        let urlParams = new URLSearchParams(window.location.search);
-        let number = urlParams.get('number');
-        let account = urlParams.get('account');
-        let name = urlParams.get('name');
-        let phone = urlParams.get('phone');
-        let pick01 = urlParams.get('datePick01');
-        let pick02 = urlParams.get('datePick02');
+    // updated() {
+    //     const self = this;
+    //     let urlParams = new URLSearchParams(window.location.search);
+    //     let number = urlParams.get('number');
+    //     let account = urlParams.get('account');
+    //     let name = urlParams.get('name');
+    //     let phone = urlParams.get('phone');
+    //     let pick01 = urlParams.get('datePick01');
+    //     let pick02 = urlParams.get('datePick02');
 
-        $.ajax({
-            url: '../PHP/backStage/points/pointsQuery.php',
-            type: 'POST',
-            data: { number, account, name, phone, pick01, pick02 },
-            success: function (res) {
-                self.tableData01 = res.pointsOfMember;
-                self.tableData02 = res.pointsUsing;
-                self.tableData03 = res.pointsIssance;
-                console.log(res);
-                // console.log(res.pointsOfMember[0][0])
-            },
-            error: function (res) {
-                console.log("回傳失敗！");
-                console.log(res);
-            },
-            dataType: "JSON",
-        });
-    },
+    //     $.ajax({
+    //         url: '../PHP/backStage/points/pointsQuery.php',
+    //         type: 'POST',
+    //         data: { number, account, name, phone, pick01, pick02 },
+    //         success: function (res) {
+    //             self.tableData01 = res.pointsOfMember;
+    //             self.tableData02 = res.pointsUsing;
+    //             self.tableData03 = res.pointsIssance;
+    //             console.log(res);
+    //         },
+    //         error: function (res) {
+    //             console.log("回傳失敗！");
+    //             console.log(res);
+    //         },
+    //         dataType: "JSON",
+    //     });
+    // },
 
     methods: {
-        query() {
-            const self = this;
-            let urlParams = new URLSearchParams(window.location.search);
-            let number = urlParams.get('number');
-            let account = urlParams.get('account');
-            let name = urlParams.get('name');
-            let phone = urlParams.get('phone');
-            let pick01 = urlParams.get('datePick01');
-            let pick02 = urlParams.get('datePick02');
+        // query() {
+        //     const self = this;
+        //     let urlParams = new URLSearchParams(window.location.search);
+        //     let number = urlParams.get('number');
+        //     let account = urlParams.get('account');
+        //     let name = urlParams.get('name');
+        //     let phone = urlParams.get('phone');
+        //     let pick01 = urlParams.get('datePick01');
+        //     let pick02 = urlParams.get('datePick02');
 
-            $.ajax({
-                url: '../PHP/backStage/points/pointsQuery.php',
-                type: 'POST',
-                data: { number, account, name, phone, pick01, pick02 },
-                success: function (res) {
-                    self.tableData01 = res.pointsOfMember;
-                    self.tableData02 = res.pointsUsing;
-                    self.tableData03 = res.pointsIssance;
-                    console.log(res);
-                    // console.log(res.pointsOfMember[0][0])
-                },
-                error: function (res) {
-                    console.log("回傳失敗！");
-                    console.log(res);
-                },
-                dataType: "JSON",
-            });
-        },
+        //     $.ajax({
+        //         url: '../PHP/backStage/points/pointsQuery.php',
+        //         type: 'POST',
+        //         data: { number, account, name, phone, pick01, pick02 },
+        //         success: function (res) {
+        //             self.tableData01 = res.pointsOfMember;
+        //             self.tableData02 = res.pointsUsing;
+        //             self.tableData03 = res.pointsIssance;
+        //             console.log(res);
+        //         },
+        //         error: function (res) {
+        //             console.log("回傳失敗！");
+        //             console.log(res);
+        //         },
+        //         dataType: "JSON",
+        //     });
+        // },
         update() {
             const self = this;
             let urlParams = new URLSearchParams(window.location.search);
