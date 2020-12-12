9d558cd830d81b6850ce18d2c39cf3f15d74a0a4
Jiang-Chiang <republicmarch@gmail.com>
2020-12-12 12:27:16 +0800

    20121202break

--- js/22/financeDetailsAjaxVue.js
+++ js/22/financeDetailsAjaxVue.js
@@ -10,36 +10,32 @@ const app = new Vue({
     mounted() {
 
         const self = this;
-        let DONATION_ID = $("input[name='DONATION_ID']").val()
-        let name = $("input[name='name']").val()
-        let email = $("input[name='email']").val()
-        let pID_tID = $("input[name='pID_tID']").val()
-        let dateStart = $("input[name='dateStart']").val()
-        let dateEnd = $("input[name='dateEnd']").val()
+        let urlParams = new URLSearchParams(window.location.search);
+        let DONATION_ID = urlParams.get('DONATION_ID');
+        let name = urlParams.get('name');
+        let email = urlParams.get('email');
+        let pID_tID = urlParams.get('pID_tID');
+        let dateStart = urlParams.get('dateStart');
+        let dateEnd = urlParams.get('dateEnd');
 
         $.ajax({
             url: '../PHP/backStage/finance/financeQuery.php',
             type: 'POST',
             data: { DONATION_ID, name, email, pID_tID, dateStart, dateEnd },
             success: function (res) {
-                self.donationDetals = res.donationDetals;
                 self.donationLog = res.donationLog;
 
-                for (let i = 0; i < res.donationLog.length; i++) {
-                    let donationPlan = res.donationLog[i].DONATION_PLAN;
+                var rDD = res.donationDetals;
 
-                    if (donationPlan == 1) {
-                        res.donationLog[i].DONATION_PLAN = "單次扣款";
-                    } else if (donationPlan == 2) {
-                        res.donationLog[i].DONATION_PLAN = "定期捐款";
-                    } else {
-                        res.donationLog[i].DONATION_PLAN = "資料錯誤";
+                for (let i = 0; i < rDD.length; i++) {
+                    if (rDD[i].DONATION_ID = DONATION_ID) {
+                        self.donationDetals = [];
+                        self.donationDetals.push(rDD[i]);
                     }
-
                 }
 
                 console.log(res);
-                console.log(res.donationDetals);
+                console.log(self.donationDetals);
                 console.log(res.donationLog);
             },
             error: function (res) {
