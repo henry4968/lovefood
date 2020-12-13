bf4b109f6554f32deb89bdf80627508d200c84a5
Jiang-Chiang <republicmarch@gmail.com>
2020-12-13 11:54:32 +0800

    20121306update

--- PHP/Frontend/donation.php
+++ PHP/Frontend/donation.php
@@ -2,9 +2,13 @@
 
 include("./Lib/connection.php");
 
-$sqlStatment = "INSERT INTO DONATION(DONATION_ID,DATE,DONATION_PLAN,DONATION_METHOD,AMOUNT,NAME,NATIONALITY,PERSONAL_ID_OR_TAX_ID,BIRTHDAY,ADDRESS,EMAIL,GENDER,REMARKS,RECEIPT_TITLE,PERSONAL_ID_OR_TAX_ID_OF_RECEIPT,DELIVERY_METHOD) VALUE ('DN0000004',NOW(),?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+$sqlStatment = "INSERT INTO 
+DONATION(DONATION_ID,DATE,DONATION_PLAN,DONATION_METHOD,AMOUNT,NAME,NATIONALITY,PERSONAL_ID_OR_TAX_ID,BIRTHDAY,ADDRESS,EMAIL,GENDER,REMARKS,RECEIPT_TITLE,PERSONAL_ID_OR_TAX_ID_OF_RECEIPT,DELIVERY_METHOD)
+ VALUE ('DN0000004',NOW(),?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+$sqlSelect = "SELECT * FROM DONATION";
 
 $sqlStatment = $pdo->prepare($sqlStatment);
+$sqlSelect = $pdo->prepare($sqlSelect);
 
 $sqlStatment->bindValue(1,$_POST["donationPlan"]);
 $sqlStatment->bindValue(2,$_POST["donationMethod"]);
@@ -14,7 +18,7 @@ $sqlStatment->bindValue(5,$_POST["nationality"]);
 $sqlStatment->bindValue(6,$_POST["pID_tID"]);
 $sqlStatment->bindValue(7,$_POST["birthday"]);
 $sqlStatment->bindValue(8,$_POST["address"]);
-$sqlStatment->bindValue(9,$_POST["emal"]);
+$sqlStatment->bindValue(9,$_POST["email"]);
 $sqlStatment->bindValue(10,$_POST["gender"]);
 $sqlStatment->bindValue(11,$_POST["remarks"]);
 $sqlStatment->bindValue(12,$_POST["receiptTitle"]);
@@ -22,9 +26,10 @@ $sqlStatment->bindValue(13,$_POST["receipt_pID_tID"]);
 $sqlStatment->bindValue(14,$_POST["deliveryMethod"]);
 
 $sqlStatment->execute();
+$sqlSelect->execute();
 
-$Data = $sqlStatment->fetchAll(PDO::FETCH_ASSOC);
+$All = $sqlSelect->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($Data);
+print_r($All);
 
 ?>
