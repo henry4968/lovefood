9d9ed118ff9f64e145065e68041065f4131a3a2f
j23063519 <j23063519@gmail.com>
2020-12-13 22:00:52 +0800

    up12132200

--- PHP/Frontend/uploadImg.php
+++ PHP/Frontend/uploadImg.php
@@ -13,21 +13,22 @@
  
   //字串以MIME BASE64編碼，在Base64中的可列印字元包括字母A-Z、a-z、數位0-9，這樣共有62個字元，此外兩個可列印符號在不同的系統中而不同
   //Base64常用於在通常處理文字資料的場合，表示、傳輸、儲存一些二進制資料
-  // 加密函式為base64_encode ( string $data)
+  // 加密函式為base64_encode (string $data)
   // $data 為你將加密的資料
   $img = base64_encode($img);
 
     
 
-    $sql = "UPDATE member SET MEMBER_AVATAR = ? where MEMBER_ID = ?";
+  $sql = "UPDATE member SET MEMBER_AVATAR = ? where MEMBER_ID = ?";
 
-    //執行
-    $statement = $Util->getPDO()->prepare($sql);
+  //執行
+  $statement = $Util->getPDO()->prepare($sql);
 
-    //給值
-    $statement->bindValue(1, $img);
-    $statement->bindValue(2, $Member);
-    $statement->execute();
-    $data = $statement->fetchAll();    
+  //給值
+  $statement->bindValue(1, $img);
+  $statement->bindValue(2, $Member);
+  $statement->execute();
+  $data = $statement->fetchAll();    
 
+  print_r($data);
 ?>
--- js/03/memberInformation.js
+++ js/03/memberInformation.js
@@ -416,7 +416,24 @@ Vue.component('member', {
         });
       }
 
-      
+      // 建立資料表單
+      // 為表單資料中的欄位/值建立相對應的的鍵/值對（key/value）集合，之後便可使用 XMLHttpRequest.send() 方法來送出資料。它在編碼類型設定為 multipart/form-data 時會採用與表單相同的格式送出。
+      let data = new FormData();//new FormData() 固定語法
+      // FormData.append()
+      // 追加新值到 FormData 物件已有的對應鍵上；若該鍵不存在，則為其追加新的鍵。
+      data.append('img', (image.src));
+
+      let config = {
+        header: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+
+      axios.post('../PHP/Frontend/uploadImg.php', data, config).then(function (response) {
+        data = response.data;
+        // console.log(response);
+        console.log(data);
+      });
     },
     // 傳入父層，控制class
     sync() {
