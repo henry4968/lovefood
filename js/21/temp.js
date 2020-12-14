abbde2e1ff8224cdcf1007917b264e7e88540f27
yuria1215 <takaii666@gmail.com>
2020-12-15 00:50:53 +0800

    1215

---
+++ js/21/temp.js
@@ -0,0 +1,14 @@
+// $(function () {
+
+
+//     $(window).on('scroll', function () {
+//         let scrollWin = $(window).scrollTop();
+//         let bread = $('#breadImg').offset().top - ($(window).innerHeight() / 2);
+//         if (scrollWin >= bread) {
+//             $('.breadLine').css({
+//                 animation: 'widthChange 2s linear forwards',
+//             });
+//         }
+
+//     });
+// });
