192f4b3c15ae3a9565e0c233f6ba0a8c3f4ad5bc
yuria1215 <takaii666@gmail.com>
2020-12-18 21:36:52 +0800

    12182136

--- PHP/Frontend/Lib/connection.php
+++ PHP/Frontend/Lib/connection.php
@@ -2,8 +2,8 @@
 
 $db_host = "localhost";
 $db_user = "root";
-// $db_pass = "password";
-$db_pass = "SQL1933";
+$db_pass = "password";
+// $db_pass = "SQL1933";
 $db_select = "Lovefood";
        
 $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
