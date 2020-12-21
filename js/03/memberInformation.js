36a1b718c1755558074dfb32c99f5b42859d4d4e
j23063519 <j23063519@gmail.com>
2020-12-21 13:13:33 +0800

    up12211313

--- js/03/memberInformation.js
+++ js/03/memberInformation.js
@@ -831,7 +831,7 @@ Vue.component('order', {
     cancelorder(e) {
       // 找到點擊的該筆訂單編號
       me = e.target;
-      console.log(me.parentElement.parentElement.parentElement.querySelector('.catalogBorder'));
+      // console.log(me.parentElement.parentElement.parentElement.querySelector('.catalogBorder'));
 
       // 建立資料表單
       // 為表單資料中的欄位/值建立相對應的的鍵/值對（key/value）集合，之後便可使用 XMLHttpRequest.send 方法來送出資料。它在編碼類型設定為 multipart/form-data 時會採用與表單相同的格式送出。
