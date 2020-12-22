5796dc9efb6419792f5cbea72fba109d4a4d7c34
Jiang-Chiang <republicmarch@gmail.com>
2020-12-22 14:47:26 +0800

    20122205update

--- PHP/backStage/member/seller/sellerIndex.php
+++ PHP/backStage/member/seller/sellerIndex.php
@@ -1,9 +1,9 @@
 <?php
      include("../../Lib/UtilClass.php");
      $Util = new UtilClass();
-     $sql = "SELECT * FROM Lovefood.SUPPLIER WHERE SUPPLIER_ID = 'SP0001'";
+     $sql = "SELECT * FROM Lovefood.SUPPLIER WHERE SUPPLIER_ID = ?";
      $statement = $Util->getPDO()->prepare($sql);
-    //  $statement->bindValue(1,$_POST['sellerNum']);
+     $statement->bindValue(1,$_POST['sellerNum']);
      $statement->execute();
      $data = $statement->fetchAll(PDO::FETCH_ASSOC);
      print json_encode($data);
