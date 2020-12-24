4ec7a9558d3a9d8f24388200bf9dcd1317555896
Jiang-Chiang <republicmarch@gmail.com>
2020-12-24 21:13:24 +0800

    201224Utilclass整合

---
+++ PHP/Frontend/Lib/frontendUtilClass.php
@@ -0,0 +1,20 @@
+<?php
+class UtilClass
+{ //取得PDO物件
+    function getPDO()
+    {
+        
+        $db_host = "localhost";
+        $db_user = "root";
+        $db_pass = "SQL1933";
+        $db_select = "Lovefood";
+
+        //建立資料庫連線物件
+        $dsn = "mysql:host=" . $db_host . ";dbname=" . $db_select;
+
+        //建立PDO物件，並放入指定的相關資料
+        $pdo = new PDO($dsn, $db_user, $db_pass);
+
+        return $pdo;
+    }
+}
