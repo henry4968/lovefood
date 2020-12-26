ea4bf0fc38f03f144286de0e778d8b3f618f2cb3
j23063519 <j23063519@gmail.com>
2020-12-26 19:08:10 +0800

    up12261908

--- js/07/07.js
+++ js/07/07.js
@@ -281,41 +281,6 @@ let all = new Vue({
 });
 
 //////////////輪播////////////
-$(document).ready(function () {
-    $('#carouselist').lightSlider({
-        item: 4,
-        loop: true,
-        auto: true,
-        slideMove: 1,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed: 600,
-        responsive: [
-            {
-                breakpoint: 1000,
-                settings: {
-                    item: 3,
-                    slideMove: 1,
-                    slideMargin: 6,
-                }
-            },
-            {
-                breakpoint: 765,
-                settings: {
-                    item: 2,
-                    slideMove: 1
-                }
-            },
-            {
-                breakpoint: 483,
-                settings: {
-                    item: 1,
-                    slideMove: 1
-                }
-            }
-
-        ]
-    });
-});
 
 //////////vue//////////
 
