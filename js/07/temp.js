fc5ebc2f5f32e6515f8dbf270f030592dacfc841
j23063519 <j23063519@gmail.com>
2020-12-27 12:50:23 +0800

    up12271250

--- js/07/temp.js
+++ js/07/temp.js
@@ -105,41 +105,17 @@ const main = new Vue({
                 price: item.PRODUCT_SELLING_PRICE,
                 id: item.PRODUCT_ID,
                 img: item.PRODUCT_IMG,
-                exp: item.PRODUCT_EXP_DATE,
-                address: item.SUPPLIER_ADDRESS,
+                exp: item.PRODUCT_EXP_DATE
             }
             let cartAllItems = JSON.parse(localStorage.getItem('itemStorage'));
             console.log(cartAllItems);
             if (item.quantity > 0) {
-                if (cartAllItems == null) { //不存在localstorage
-                    self.itemStorage.push(produ);
-                    self.itemQty++;
-                    localStorage.setItem('itemStorage', JSON.stringify(self.itemStorage));
-                } else {
-                    var temp = cartAllItems.some(function (item) {
-                        return item.name == produ.name;
-                    })
-                    if (temp) {
-                        for (i = 0; i < cartAllItems.length; i++) {
-                            if (cartAllItems[i].name == produ.name) {
-                                cartAllItems[i].qty += item.quantity
-                            }
-                            // alert('相同')
-                        }
-                    } else {
-                        cartAllItems.push(produ);
-                        self.itemQty++;//購物車圖標
-                        // alert('不同')
-                    }
-                    localStorage.setItem('itemStorage', JSON.stringify(cartAllItems));
-                    console.log(self.itemStorage);
-                    // alert('寫入')
-                }
-                alert('成功加入購物車');
-            } else {
-                alert('請選擇數量');
+                // localStorage
+                self.itemStorage.push(produ);
+                self.itemQty++;
             }
-            item.quantity = 0;
+            console.log(self.itemStorage);
+            localStorage.setItem('itemStorage', JSON.stringify(self.itemStorage));
         },
         queryData(url, data = null) {
             const self = this
@@ -412,10 +388,22 @@ const main = new Vue({
             })
         }, 1000);
         //換頁載入＝＝＝＝＝
-        let cartAllItems = JSON.parse(localStorage.getItem('itemStorage'));
-        if (cartAllItems) {
-            self.itemQty = cartAllItems.length
-        }
+        // $.ajax({
+        //     url:'../PHP/Frontend/EC_07/storeCard.php',
+        //     type: 'POST',
+        //         success: function (res) {
+        //             // let aaa = JSON.parse(res);
+        //             // console.log(res);
+        //             for (let index = 0; index < res.length; index++) {
+        //                 res[index].quantity = 0
+        //             }
+        //             self.tableData = res;
+        //         },
+        //         error: function (res) {
+        //             console.log('bbb');
+        //         },
+        //     dataType:'JSON',
+        // })
 
         // 看看是一般會員或是賣家會員
         this.checklogin();
--- js/16/store_04.js
+++ js/16/store_04.js
@@ -30,6 +30,17 @@ new Vue({
         //localStorage取值
         let test = JSON.parse(localStorage.getItem("newStorage"));
         this.newStorage = test;
+        // console.log(this.newStorage);
+        // console.log(this.newStorage[0].goodList[0].id);
+        // console.log(this.newStorage[0].goodList[0].qty);
+
+        for (let i = 0; i < this.newStorage.length; i++) {
+            for (let j = 0; j < this.newStorage[i].goodList.length; j++) {
+                // console.log(this.newStorage[i].goodList[j].id);
+                // console.log(this.newStorage[i].goodList[j].qty);
+            }
+        }
+
         //換頁載入＝＝＝＝＝
         let cartAllItems = JSON.parse(localStorage.getItem('itemStorage'));
         if (cartAllItems) {
@@ -39,8 +50,8 @@ new Vue({
         this.discountPoints = JSON.parse(localStorage.getItem('discountPoints'));
         this.sessionId = JSON.parse(localStorage.getItem('memberId'));
 
-        console.log(this.sessionId);
-        console.log(this.discountPoints);
+        // console.log(this.sessionId);
+        // console.log(this.discountPoints);
     },
     updated() {
         // 看看是一般會員或是賣家會員
@@ -220,33 +231,37 @@ new Vue({
                 for (var i in this.newStorage[index].goodList) {
                     total += this.newStorage[index].goodList[i].price * this.newStorage[index].goodList[i].qty;
                 }
-                console.log(i);
+                // console.log(i);
             }
             return total;
         },
-        mrtStation() {
-            siteNum = [];
-            iNum = [];
-            site = ['MPS00001', 'MPS00002', 'MPS00003', 'MPS00004', 'MPS00005', 'MPS00006', 'MPS00007', 'MPS00008', 'MPS00009', 'MPS00010', 'MPS00011', 'MPS00012', 'MPS00013', 'MPS00014', 'MPS00015'];
-            newSite = ['捷運市政府站', '捷運南京復興站', '捷運忠孝復興站', '捷運大安站', '捷運中山站', '捷運台北車站', '捷運西門站', '捷運中正紀念堂站', '捷運大橋頭站', '捷運民權西路站', '捷運石牌站', '捷運內湖站', '捷運南港站', '捷運古亭站', '捷運六張犁站'];
-            for (i in this.newStorage) {
-                siteNum.push(this.newStorage[i].pickUpSite)
-            }
-            for (i in siteNum) {
-                iNum.push(site.indexOf(siteNum[i]));
-            }
-            let abc = newSite.filter(function (item, index, array) {
-                for (i in iNum) {
-                    if (index == iNum[i]) {
-                        return true
-                    }
-                }
-            })
-            return abc;
-        }
     }
 
 });
 
+
+// $(function () {
+
+//     // 開啟 Modal 彈跳視窗
+//     $("button.btn_modal").on("click", function () {
+//         $("div.overlay").addClass("-on");
+//     });
+
+//     // 關閉 Modal
+//     $("button.btn_modal_close").on("click", function () {
+//         $("div.overlay").addClass("-opacity-zero");
+
+//         // 設定隔一秒後，移除相關 class
+//         setTimeout(function () {
+//             $("div.overlay").removeClass("-on -opacity-zero");
+//         }, 1000);
+//     });
+
+//     //點擊其他地方關閉 Madal
+//     $("div.overlay").on("click", function () {
+//         $("div.overlay").removeClass("-on -opacity-zero");
+//     });
+
+// });
 
 
