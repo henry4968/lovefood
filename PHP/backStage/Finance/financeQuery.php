74a7aa937c87495d5b187f3c73ec759f20055220
j23063519 <j23063519@gmail.com>
2021-01-12 18:38:00 +0800

    up01121837

--- PHP/backStage/Finance/financeQuery.php
+++ PHP/backStage/Finance/financeQuery.php
@@ -4,34 +4,34 @@
     $Util = new UtilClass();
 
     $sqlTotalSelling = "SELECT T1.ORDER_ID, T1.ORDER_DETAIL_QUANTITY, PD.PRODUCT_SELLING_PRICE, PD.SUPPLIER_ID_for_PD
-                        FROM (SELECT * FROM `order` OD JOIN ORDER_DETAIL ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1 
+                        FROM (SELECT * FROM `order` OD JOIN order_detail ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1 
                         JOIN product PD ON T1.PRODUCT_ID_for_ODD = PD.PRODUCT_ID
                         WHERE T1.ORDER_DATE between ? and ? and PD.SUPPLIER_ID_for_PD like ? and T1.ORDER_STATUS = 1";
 
     $sqlDailySelling = "SELECT PD.SUPPLIER_ID_for_PD as SUPPLIER, year(T1.ORDER_DATE) as `YEAR`, month(T1.ORDER_DATE) as `MONTH`, 
                         day(T1.ORDER_DATE) as `DAY`, sum(PD.PRODUCT_SELLING_PRICE * T1.ORDER_DETAIL_QUANTITY) as DAILY_SELLING 
-                        FROM (SELECT * FROM `order` OD JOIN ORDER_DETAIL ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1 
+                        FROM (SELECT * FROM `order` OD JOIN order_detail ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1 
                         JOIN product PD ON T1.PRODUCT_ID_for_ODD = PD.PRODUCT_ID WHERE PD.SUPPLIER_ID_for_PD = ? and T1.ORDER_STATUS = 1 
                         and T1.ORDER_DATE between ? and ? GROUP BY year(T1.ORDER_DATE), month(T1.ORDER_DATE), day(T1.ORDER_DATE)";
 
     $sqlTotalOrder = "SELECT PD.SUPPLIER_ID_for_PD, count(T1.ORDER_ID) as TOTAL_ORDER
-                        FROM (SELECT * FROM `order` OD JOIN ORDER_DETAIL ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1
+                        FROM (SELECT * FROM `order` OD JOIN order_detail ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1
                         JOIN product PD ON T1.PRODUCT_ID_for_ODD = PD.PRODUCT_ID WHERE T1.ORDER_DATE between ? and ? 
                         and PD.SUPPLIER_ID_for_PD like ? and T1.ORDER_STATUS = 1 GROUP BY PD.SUPPLIER_ID_for_PD";
 
     $sqlTotalSoldGoods = "SELECT T1.ORDER_ID, T1.ORDER_DATE, T1.PRODUCT_ID_for_ODD, PD.PRODUCT_NAME, T1.ORDER_DETAIL_QUANTITY
-                            FROM (SELECT * FROM `order` OD JOIN ORDER_DETAIL ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1
+                            FROM (SELECT * FROM `order` OD JOIN order_detail ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1
                             JOIN product PD ON T1.PRODUCT_ID_for_ODD = PD.PRODUCT_ID WHERE T1.ORDER_DATE between ? and ?
                             and PD.SUPPLIER_ID_for_PD like ? and T1.ORDER_STATUS = 1";
 
     $sqlMostPopularGood = "SELECT T1.PRODUCT_ID_for_ODD, PD.PRODUCT_NAME, sum(T1.ORDER_DETAIL_QUANTITY) as Total_QUANTITY
-                            FROM (SELECT * FROM `order` OD JOIN ORDER_DETAIL ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1
+                            FROM (SELECT * FROM `order` OD JOIN order_detail ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1
                             JOIN product PD ON T1.PRODUCT_ID_for_ODD = PD.PRODUCT_ID WHERE T1.ORDER_DATE between ? and ? 
                             and PD.SUPPLIER_ID_for_PD like ? and T1.ORDER_STATUS = 1 
                             GROUP BY T1.PRODUCT_ID_for_ODD ORDER BY Total_QUANTITY desc LIMIT 1";
 
     $sqlMostUnpopularGood = "SELECT T1.PRODUCT_ID_for_ODD, PD.PRODUCT_NAME, sum(T1.ORDER_DETAIL_QUANTITY) as Total_QUANTITY
-                                FROM (SELECT * FROM `order` OD JOIN ORDER_DETAIL ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1
+                                FROM (SELECT * FROM `order` OD JOIN order_detail ODD ON OD.ORDER_ID = ODD.ORDER_ID_for_ODD) as T1
                                 JOIN product PD ON T1.PRODUCT_ID_for_ODD = PD.PRODUCT_ID WHERE T1.ORDER_DATE between ? and ? 
                                 and PD.SUPPLIER_ID_for_PD like ? and T1.ORDER_STATUS = 1 
                                 GROUP BY T1.PRODUCT_ID_for_ODD ORDER BY Total_QUANTITY asc LIMIT 1";
@@ -147,5 +147,3 @@
         $arr = explode('-',$string);
         return (isset($arr[0])) && isset($arr[1]) && isset($arr[2]) && checkdate($arr[1],$arr[2],$arr[0])? true : false;
     }
-
-?>
